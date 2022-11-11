4682ed38041215faa4e241be5f5fc21f3ee8fae7
James Montemagno <jamont@microsoft.com>
2022-11-11 07:55:16 -0800

    Add GA

--- MonkeyApp1/Program.cs
+++ MonkeyApp1/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
