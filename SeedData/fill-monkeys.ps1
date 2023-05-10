847296b262d4827a5c637d0f3917f572b5b64530
James Montemagno <james.montemagno@gmail.com>
2023-05-10 11:03:32 -0700

    Add files via upload

---
+++ SeedData/fill-monkeys.ps1
@@ -0,0 +1,36 @@
+Param(
+    [parameter(Mandatory=$false)][string]$baseUrl = "https://localhost:7179/"
+)
+
+class Monkey 
+{
+    [string] $Name
+    [string] $Location
+    [string] $Details
+    [string] $Image
+    [int] $Population
+    [double] $Latitude
+    [double] $Longitude
+}
+
+function SimulateRequest {
+    Param(
+     [int]$sleep = 100,
+     [string]$baseUrl
+    )
+    
+    $feeds = [System.Collections.Generic.List[Monkey]](Get-Content './monkeys.json' | Out-String | ConvertFrom-Json)
+    
+    $feeds | ForEach-Object -ThrottleLimit 20 -Parallel {
+        Write-Host "> Requesting feed" $_.Name 
+
+        Invoke-WebRequest -Method POST -Uri $using:baseUrl"api/Monkey" `
+                        -Body ($_|ConvertTo-Json) `
+                        -ContentType application/json
+
+        [System.Threading.Thread]::Sleep($sleep)
+    } 
+}
+
+
+SimulateRequest -baseUrl $baseUrl
